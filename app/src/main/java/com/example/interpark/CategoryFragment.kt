e180534eea27809ac4fc2bdf0d718763a51e04e3
nina <junghyunpark2001@naver.com>
2024-12-23 18:01:24 +0900

    feat/fragments 구현

---
+++ app/src/main/java/com/example/interpark/CategoryFragment.kt
@@ -0,0 +1,16 @@
+package com.example.interpark
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CategoryFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+}
