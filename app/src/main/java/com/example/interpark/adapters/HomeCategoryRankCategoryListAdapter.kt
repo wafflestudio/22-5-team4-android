fcd182e279bdc0bccf6f30d15d6e1fad8dd311b5
qdrptd <qdrptd@gmail.com>
2025-01-03 20:01:07 +0900

    feat/Home_CategoryRank_CategoryList

--- app/src/main/java/com/example/interpark/adapters/HomeCategoryRankCategoryListAdapter.kt
+++ app/src/main/java/com/example/interpark/adapters/HomeCategoryRankCategoryListAdapter.kt
@@ -3,30 +3,43 @@ package com.example.interpark.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.interpark.R
+import com.example.interpark.data.Category
 import com.example.interpark.data.CategoryItem
 
-class HomeCategoryAdapter(
-    private val items: List<CategoryItem.Category>,
-    private val onCategoryClick: (CategoryItem.Category) -> Unit
+data class HomeCategoryRankCategory(
+    val category: Category,
+    val selected: Boolean
+)
+
+class HomeCategoryRankCategoryListAdapter(
+    private val items: List<HomeCategoryRankCategory>,
+    private val onCategoryClick: (Category) -> Unit
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val TYPE_CLICKED = 0
+        const val TYPE_NOT_CLICKED = 1
+    }
+
 
     class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val name: TextView = itemView.findViewById(R.id.categoryName)
-        private val icon: ImageView = itemView.findViewById(R.id.categoryIcon)
 
-        fun bind(item: CategoryItem.Category, onClick: (CategoryItem.Category) -> Unit) {
+        fun bind(item: Category, onClick: (Category) -> Unit) {
             name.text = item.name
-            icon.setImageResource(item.iconResId)
             itemView.setOnClickListener { onClick(item) }
         }
     }
 
+    override fun getItemViewType(position: Int): Int{
+        return if(items[position].selected) TYPE_CLICKED else TYPE_NOT_CLICKED
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_home_category, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_home_category_rank_category_list, parent, false)
         return CategoryViewHolder(view)
 
     }
@@ -34,7 +47,7 @@ class HomeCategoryAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val categoryItem = items[position]
         if(holder is CategoryViewHolder){
-            holder.bind(categoryItem, onCategoryClick)
+            holder.bind(categoryItem.category, onCategoryClick)
         }
     }
 
